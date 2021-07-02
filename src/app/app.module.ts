c3cf878816979fd4ee8dfb2ba86d86a6e7bc6bd0
Chiki-Paperoni <tv81eugene@gmail.com>
2021-07-02 14:00:38 +0300

    separated nav + fonts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
