ebb1327736018811dbb66fc9e39ecbf53ec000fd
Chiki-Paperoni <tv81eugene@gmail.com>
2021-07-14 11:17:25 +0300

    adaptive + mobile popup

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -10,6 +10,10 @@ import { PostOrderService } from '../post-order.service';
 })
 export class NavComponent implements OnInit {
   isPopupShown = false;
+  popupsSuccess = false;
+  mobileBgStyle = '';
+  mobileMenuStyle = '';
+
   orderForm = this.formBuilder.group({
     name: '',
     phone: '',
@@ -25,6 +29,19 @@ export class NavComponent implements OnInit {
       phone: '+380',
     });
   }
+  TogleMobileMenu(): void {
+    if (this.mobileBgStyle === '') {
+      this.mobileBgStyle = 'background-color: #2c2431;min-height:100vh';
+      document.body.style.overflow = 'hidden';
+
+      this.mobileMenuStyle = 'display:flex;';
+    } else {
+      this.mobileBgStyle = '';
+      document.body.style.overflow = '';
+
+      this.mobileMenuStyle = '';
+    }
+  }
 
   showPopup(): void {
     this.isPopupShown = true;
@@ -34,15 +51,16 @@ export class NavComponent implements OnInit {
     this.isPopupShown = false;
     document.body.style.overflow = '';
   }
-  checkChange(e: InputEvent) {
-    console.log(e);
-  }
   onSubmit(): void {
-    this.service.postOrder(this.orderForm.value).subscribe((data) => {
-      this.orderForm.setValue({
-        name: '',
-        phone: '+380',
-      });
-    });
+    this.service.postOrder(this.orderForm.value).subscribe(
+      (data) => {
+        this.orderForm.setValue({
+          name: '',
+          phone: '+380',
+        });
+        this.popupsSuccess = true;
+      },
+      (error) => {}
+    );
   }
 }
