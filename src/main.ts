fdf1f7177ae29a600827d198ad77e5d7cc6ba479
Chiki-Paperoni <tv81eugene@gmail.com>
2021-10-21 20:01:30 +0300

    vercel

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -9,6 +10,19 @@ if (environment.production) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
 });
+
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .then((moduleRef) => {
+//     const applicationRef = moduleRef.injector.get(ApplicationRef);
+//     const componentRef = applicationRef.components[0];
+//     // allows to run `ng.profiler.timeChangeDetection();`
+//     enableDebugTools(componentRef);
+//   })
+//   .catch((err) => console.log(err));
+
+// RUN THIS IN DEV TOOLS  ng.profiler.timeChangeDetection()
