bc65e9a208f681b187722b8b9e7c00d8880148e9
Chiki-Paperoni <tv81eugene@gmail.com>
2021-10-24 20:20:57 +0300

    api folder for serverless functions

---
+++ api/post.js
@@ -0,0 +1,23 @@
+const TELEGRAM =
+  "https://api.telegram.org/bot1730980288:AAGky2y9SAWak9-ygjfEKNnA5eroJQYIz_Q/sendMessage?chat_id=-1001563698953&parse_mode=html&text=";
+export function post(req, res) {
+  let body = "";
+  req.on("data", (data) => {
+    body += data;
+  });
+  req.on("end", () => {
+    axios
+      .post(TELEGRAM + encodeURIComponent(body))
+      .then(() => {
+        res.status(200);
+        res.end();
+      })
+      .catch((error) => {
+        console.error(
+          "An error occured while sendin data to telegram bot:",
+          error
+        );
+        res.end();
+      });
+  });
+}
